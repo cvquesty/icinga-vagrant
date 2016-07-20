3abd1b2572970e2be951a0ec6152dec8fac9d869
Jerald Sheets <questy@gmail.com>
2016-07-20 16:51:39 -0700

    Refactoring for syntax & style

--- icinga1x/modules/icinga/manifests/init.pp
+++ icinga1x/modules/icinga/manifests/init.pp
@@ -1,7 +1,7 @@
 # Class: icinga, icinga-idoutils-libdbi-mysql
 #
 #   Install Icinga and IDOUtils incl DB Setup from snapshot rpms
-#   
+#
 #   Copyright (C) 2014-present Icinga Development Team (http://www.icinga.org/)
 #
 # Parameters:
@@ -17,93 +17,96 @@
 #
 
 class icinga {
-  include icinga-rpm-snapshot
+  include ::icinga-rpm-snapshot
 
   package { 'icinga':
-    ensure => installed,
+    ensure  => 'installed',
     require => Class['icinga-rpm-snapshot'],
-    alias => 'icinga'
+    alias   => 'icinga',
   }
 
   package { 'icinga-doc':
-    ensure => installed,
+    ensure  => 'installed',
+    alias   => 'icinga-doc',
     require => Class['icinga-rpm-snapshot'],
-    alias => 'icinga-doc'
   }
 
   service { 'icinga':
-    enable => true,
-    ensure => running,
+    ensure     => 'running',
+    enable     => true,
     hasrestart => true,
-    alias => 'icinga',
-    require => Package['icinga']
+    alias      => 'icinga',
+    require    => Package['icinga'],
   }
 
-  file { "/etc/icinga/modules/*":
-    notify => Service['icinga']
+  file { '/etc/icinga/modules/*':
+    notify => Service['icinga'],
   }
 }
 
-class icinga-idoutils-libdbi-mysql {
-  include icinga-rpm-snapshot
-  include mysql
+# Icinga IdoUtils - libDBI for MySQL
+class icinga_idoutils_libdbi_mysql {
+  include ::icinga-rpm-snapshot
+  include ::mysql
 
   package { 'icinga-idoutils-libdbi-mysql':
-    ensure => installed,
+    ensure  => 'installed',
     require => Class['icinga-rpm-snapshot'],
-    alias => 'icinga-idoutils-libdbi-mysql'
+    alias   => 'icinga-idoutils-libdbi-mysql',
   }
 
   file { '/etc/sysconfig/ido2db':
-    source => 'puppet:////vagrant/files/etc/sysconfig/ido2db',
-    require => Package['icinga-idoutils-libdbi-mysql']
+    source  => 'puppet:////vagrant/files/etc/sysconfig/ido2db',
+    require => Package['icinga-idoutils-libdbi-mysql'],
   }
 
   file { '/etc/icinga/modules/idoutils.cfg':
-    source => 'puppet:////vagrant/files/etc/icinga/modules/idoutils.cfg',
+    source  => 'puppet:////vagrant/files/etc/icinga/modules/idoutils.cfg',
     require => Package['icinga'],
-    notify => Service['icinga']
+    notify  => Service['icinga'],
   }
 
   file { '/etc/icinga/idomod-mysql.cfg':
-    source => 'puppet:////vagrant/files/etc/icinga/idomod-mysql.cfg',
+    source  => 'puppet:////vagrant/files/etc/icinga/idomod-mysql.cfg',
     require => [ Package['icinga-idoutils-libdbi-mysql'],
-		 Package['icinga'],
-		 Exec['populate-mysql-icinga-idoutils-db']
-	       ],
-    notify => Service['icinga']    
+                Package['icinga'],
+                Exec['populate-mysql-icinga-idoutils-db'],
+                ],
+    notify  => Service['icinga'],
   }
 
   file { '/etc/icinga/ido2db-mysql.cfg':
-    source => 'puppet:////vagrant/files/etc/icinga/ido2db-mysql.cfg',
+    source  => 'puppet:////vagrant/files/etc/icinga/ido2db-mysql.cfg',
     require => [ Package['icinga-idoutils-libdbi-mysql'],
-		 File['/etc/sysconfig/ido2db'],
-		 Exec['populate-mysql-icinga-idoutils-db']
-	       ],
-    notify => Service['ido2db']
+                File['/etc/sysconfig/ido2db'],
+                Exec['populate-mysql-icinga-idoutils-db']
+              ],
+    notify  => Service['ido2db'],
   }
 
   exec { 'create-mysql-icinga-idoutils-db':
-    path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless => 'mysql -uicinga -picinga icinga',
+    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+    unless  => 'mysql -uicinga -picinga icinga',
     command => 'mysql -uroot -e "CREATE DATABASE icinga; GRANT ALL ON icinga.* TO icinga@localhost IDENTIFIED BY \'icinga\';"',
-    require => Service['mysqld']
+    require => Service['mysqld'],
   }
 
   exec { 'populate-mysql-icinga-idoutils-db':
-    path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless => 'mysql -uicinga -picinga icinga -e "SELECT * FROM icinga_dbversion;" &> /dev/null',
+    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+    unless  => 'mysql -uicinga -picinga icinga -e "SELECT * FROM icinga_dbversion;" &> /dev/null',
     command => 'mysql -uicinga -picinga icinga < /usr/share/doc/icinga-idoutils-libdbi-mysql-$(rpm -q icinga-idoutils-libdbi-mysql | cut -d\'-\' -f5)/db/mysql/mysql.sql',
-    require => [ Package['icinga-idoutils-libdbi-mysql'], Exec['create-mysql-icinga-idoutils-db'] ]
+    require => [ Package['icinga-idoutils-libdbi-mysql'], Exec['create-mysql-icinga-idoutils-db'] ],
   }
-  
+
   service { 'ido2db':
-    enable => true,
-    ensure => running,
+    ensure     => 'running',
+    enable     => true,
     hasrestart => true,
-    alias => 'ido2db',
-    require => [ Package['icinga-idoutils-libdbi-mysql'], Exec['populate-mysql-icinga-idoutils-db'] ],
-    notify => Service['icinga']
+    alias      => 'ido2db',
+    require    => [ Package['icinga-idoutils-libdbi-mysql'],
+                    Exec['populate-mysql-icinga-idoutils-db'],
+                  ],
+    notify     => Service['icinga'],
   }
 }
 
