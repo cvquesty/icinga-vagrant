10d10129bb8b56ec0f92346b0932c3293bb88525
Jerald Sheets <questy@gmail.com>
2016-07-20 16:30:02 -0700

    Refactoring for syntax & style

--- icinga1x/modules/epel/manifests/key.pp
+++ icinga1x/modules/epel/manifests/key.pp
@@ -1,13 +1,12 @@
-
-
+# Defined Type to import the EPEL key
 # inspired by https://github.com/stahnma/puppet-module-epel/blob/master/manifests/rpm_gpg_key.pp
 
 define epel::key($path) {
   exec { "import-key-${name}":
-    path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    command => "rpm --import ${path}",
-    unless => "rpm -q gpg-pubkey-$(echo $(gpg --throw-keyids < ${path}) | cut --characters=11-18 | tr '[A-Z]' '[a-z]')",
-    require => File[$path],
-    logoutput => 'on_failure'
+    path      => '/bin:/usr/bin:/sbin:/usr/sbin',
+    command   => "rpm --import ${path}",
+    unless    => "rpm -q gpg-pubkey-$(echo $(gpg --throw-keyids < ${path}) | cut --characters=11-18 | tr '[A-Z]' '[a-z]')",
+    require   => File[$path],
+    logoutput => 'on_failure',
   }
 }
