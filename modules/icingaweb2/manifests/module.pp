fd6756aa5a5056769167785c52a21171092677d6
Jerald Sheets <questy@gmail.com>
2016-07-21 14:16:12 -0700

    Refactoring for Syntax and Style

--- modules/icingaweb2/manifests/module.pp
+++ modules/icingaweb2/manifests/module.pp
@@ -1,12 +1,12 @@
-
+# IcingaWeb2 Configuration Module
+#
 define icingaweb2::module (
   $builtin = false,
-  $enable = true
+  $enable  = true
 ) {
-  $repo_base_url = "https://github.com/Icinga/icingaweb2-module-"
-  $repo_url = "$repo_base_url$title"
-
-  $repo_path = "$::icingaweb2::web_module_dir/$title"
+  $repo_base_url = 'https://github.com/Icinga/icingaweb2-module-'
+  $repo_url      = "${repo_base_url}${title}"
+  $repo_path     = "${::icingaweb2::web_module_dir}/${title}"
 
   validate_string($repo_path)
 
@@ -18,15 +18,14 @@ define icingaweb2::module (
       revision => 'master',
       source   => $repo_url,
       force    => true,
-      require  => [ Package['git'], File[$::icingaweb2::web_module_dir] ]
+      require  => [ Package['git'], File[$::icingaweb2::web_module_dir] ],
     }
   }
 
-  file {
-    "$::icingaweb2::config_dir/enabledModules/$title":
-      ensure 	=> 'link',
-      target 	=> $repo_path,
-      require 	=> File["$::icingaweb2::config_dir/enabledModules"];
+  file { "${::icingaweb2::config_dir}/enabledModules/${title}":
+    ensure  => 'link',
+    target  => $repo_path,
+    require => File["${::icingaweb2::config_dir}/enabledModules"],
   }
 
 }
